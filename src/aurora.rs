2c905b5650acbf4d28897f445cb93009aa8e7abb
Jens Nockert <jens@nockert.se>
2014-10-08 02:15:02 +0200

    Non-allocating source is now available, few kinks, but generally cool

--- src/aurora.rs
+++ src/aurora.rs
@@ -1,3 +1,10 @@
+#![feature(unsafe_destructor)]
+
+pub mod channel;
+
 pub mod stream;
 pub mod file;
-pub mod source;
+
+pub trait Initialize {
+  fn initialize(&mut self);
+}
---
+++ src/channel/mod.rs
@@ -0,0 +1,189 @@
+extern crate alloc;
+
+use std::mem;
+use std::cell::UnsafeCell;
+use std::ptr::{RawPtr,RawMutPtr};
+
+use std::sync;
+use std::sync::{Arc,Semaphore};
+use std::sync::atomic::{AtomicBool,AtomicInt};
+
+struct Channel<T> {
+  rc_read: AtomicInt, rc_write: AtomicInt,
+  data: int, capacity: int,
+  read: AtomicInt, write: AtomicInt,
+  not_empty: Semaphore, not_full: Semaphore
+}
+
+#[unsafe_destructor]
+impl<T> Drop for Channel<T> {
+  fn drop(&mut self) {
+    unsafe { alloc::heap::deallocate(self.data as *mut u8, self.capacity as uint * mem::size_of::<T>(), mem::align_of::<T>()) };
+  }
+}
+
+pub struct Source<T> {
+  channel: Arc<Channel<T>>
+}
+
+impl<T> Source<T> {
+  fn new(channel: Arc<Channel<T>>) -> Source<T> {
+    return Source { channel: channel };
+  }
+
+  /// Attempts to read a value from the sink, blocking if it is empty
+  ///
+  /// Fails if there is no Sink, and no data left to read.
+  pub fn read(&mut self, f: |&T|) {
+    if self.channel.rc_write.load(sync::atomic::SeqCst) == 0 {
+      if self.channel.read.load(sync::atomic::SeqCst) == self.channel.write.load(sync::atomic::SeqCst) {
+        fail!("Sink: Source is dropped")
+      }
+    }
+
+    self.channel.not_empty.acquire();
+
+    unsafe {
+      let offset = self.channel.read.fetch_add(1, sync::atomic::SeqCst) % self.channel.capacity;
+      let ptr = mem::transmute::<int, *mut T>(self.channel.data).offset(offset);
+      let data = ptr.as_ref().unwrap();
+
+      f(data);
+    }
+
+    self.channel.not_full.release();
+  }
+}
+
+#[unsafe_destructor]
+impl<T> Drop for Source<T> {
+  fn drop(&mut self) {
+    self.channel.rc_read.fetch_sub(1, sync::atomic::SeqCst);
+  }
+}
+
+
+pub struct Sink<T> {
+  channel: Arc<Channel<T>>
+}
+
+impl<T: super::Initialize> Sink<T> {
+  fn new(channel: Arc<Channel<T>>) -> Sink<T> {
+    return Sink { channel: channel };
+  }
+
+  /// Attempts to write a value to the sink, blocking if it is full.
+  ///
+  /// Fails if there is no Source.
+  pub fn write(&mut self, f: |&mut T|) {
+    if self.channel.rc_read.load(sync::atomic::SeqCst) == 0 {
+      fail!("Sink: Source is dropped")
+    }
+
+    self.channel.not_full.acquire();
+
+    unsafe {
+      let offset = self.channel.write.fetch_add(1, sync::atomic::SeqCst) % self.channel.capacity;
+      let ptr = mem::transmute::<int, *mut T>(self.channel.data).offset(offset);
+      let data = ptr.as_mut().unwrap();
+
+      data.initialize();
+
+      f(data);
+    }
+
+    self.channel.not_empty.release();
+  }
+}
+
+#[unsafe_destructor]
+impl<T> Drop for Sink<T> {
+  fn drop(&mut self) {
+    self.channel.rc_write.fetch_sub(1, sync::atomic::SeqCst);
+  }
+}
+
+pub fn create<T: super::Initialize>(capacity: uint) -> (Sink<T>, Source<T>) {
+  let data = unsafe {
+    alloc::heap::allocate(capacity * mem::size_of::<T>(), mem::align_of::<T>())
+  };
+
+  let channel = Arc::new(Channel {
+    rc_read: AtomicInt::new(1), rc_write: AtomicInt::new(1),
+    data: unsafe { mem::transmute(data) }, capacity: capacity as int,
+    read: AtomicInt::new(0), write: AtomicInt::new(0),
+    not_empty: Semaphore::new(0), not_full: Semaphore::new(capacity as int)
+  });
+
+  let sink = Sink::<T>::new(channel.clone());
+  let source = Source::<T>::new(channel);
+
+  return (sink, source);
+}
+
+#[cfg(test)]
+mod tests {
+  struct Test { value: uint }
+
+  impl ::Initialize for Test {
+    fn initialize(&mut self) {
+      self.value = 0;
+    }
+  }
+
+  #[test]
+  fn test_basic() {
+    let (mut sink, mut source) = super::create::<Test>(1);
+
+    sink.write(|x: &mut Test| { x.value = 1 });
+    source.read(|x: &Test| { assert_eq!(x.value, 1) });
+  }
+
+  #[test]
+  fn test_threads() {
+    let (mut sink, mut source) = super::create::<Test>(1);
+
+    spawn(proc() {
+      sink.write(|x: &mut Test| { x.value = 1 });
+    });
+
+    source.read(|x: &Test| { assert_eq!(x.value, 1) });
+  }
+
+  #[test]
+  #[should_fail]
+  fn test_source_gone() {
+    let (mut sink, mut source) = super::create::<Test>(1);
+
+    drop(source);
+
+    sink.write(|x: &mut Test| { x.value = 1 });
+  }
+
+  #[test]
+  #[should_fail]
+  fn test_source_gone_threads() {
+    let (mut sink, mut source) = super::create::<Test>(1);
+
+    spawn(proc() {
+      source.read(|x: &Test| { assert_eq!(x.value, 1) });
+    });
+
+    loop { sink.write(|x: &mut Test| { x.value = 1 }) };
+  }
+
+  #[test]
+  fn test_source_works() {
+    let (mut sink, mut source) = super::create::<Test>(1);
+
+    spawn(proc() {
+      for i in range(0u, 10000) {
+        sink.write(|x: &mut Test| { x.value = i })
+      }
+    });
+
+    for i in range(0u, 10000) {
+      source.read(|x: &Test| { assert_eq!(x.value, i) });
+    }
+  }
+}
